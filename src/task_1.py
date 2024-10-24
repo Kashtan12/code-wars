184c9ed7ff48ff41f585b5d84feee1cac08d6aeb
Kashtan12 <sadullaevaamina2@gmail.com>
2024-10-25 00:51:00 +0300

    task_1 solution

--- src/task_1.py
+++ src/task_1.py
@@ -1,1 +1,12 @@
-# test
+# https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
+
+
+def bool_to_word(boolean):
+    if boolean is False:
+        return ("No")
+    if boolean is True:
+        return ("Yes")
+
+
+print(bool_to_word(False))
+print(bool_to_word(True))
